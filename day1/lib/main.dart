d68fd2387ecf61c9ba20ab5a9d598bb81df0c643
Yuchen <1900827748@qq.com>
2026-07-16 09:51:31 +0800

    chore: update flutter

--- day1/lib/main.dart
+++ day1/lib/main.dart
@@ -1,20 +1,129 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+runApp(const MaterialApp(title: "My App", home: MainApp(title: Text('Hello World!'))));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+  const MainApp({required this.title,super.key});
+
+  final Widget title;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    // Scaffold is a layout for
+    // the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+            Counter(),
+            MyButton(),
+          ],
         ),
       ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the second screen using a named route.
+        print('Engage button tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(child: Text('Engage')),
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CounterIncrementButton(onPressed: _increment),
+        const SizedBox(width: 16),
+        CounterDisplay(count: _counter),
+      ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  final int count;
+
+  const CounterDisplay({required this.count, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const CounterIncrementButton({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text('Increment'),
     );
   }
 }
---
+++ day2/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Product{
+  const Product({required this.name});
+  final String name;
+}
+
+typedef CartChangedCallback = void Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget{
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  const ShoppingListItem({required this.product, required this.inCart, required this.onCartChanged, Key? key}) : super(key: key);
+
+  Color _getColor(BuildContext context){
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context){
+    if(!inCart) return null;
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: (){
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context),),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget{
+  const ShoppingList({required this.products, Key? key}) : super(key: key);
+
+    final List<Product> products;
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList>{
+   
+  final Set<Product> _shoppingCart = {};
+
+  void _handleCartChanged(Product product, bool inCart){
+    setState(() {
+      if(!inCart){
+        _shoppingCart.add(product);
+      }else{
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body:ListView(
+        padding: const EdgeInsets.symmetric(vertical:8.0),
+        children:widget.products.map((Product product){
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+
+      )
+    );
+  }
+}
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Shopping App',
+    home: ShoppingList(
+      products: const <Product>[
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ],
+    ),
+  ));
+}
